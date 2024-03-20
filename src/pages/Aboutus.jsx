1a79e0a3d0ec5c94b0eb7b243aefd5a47e5d34f2
princesaubhagyam <prince@saubhagyam.org>
2024-03-20 19:57:05 +0530

    init

---
+++ src/pages/Aboutus.jsx
@@ -0,0 +1,125 @@
+import Footer from "../components/Footer";
+import BreadCrumbCom from "../components/BreadCrumbCom";
+import Navbar from "../components/Navbar";
+import { Box, Container, Flex, Image, Heading,Text,Button } from "@chakra-ui/react";
+import { useNavigate, NavLink as RouterLink } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+const Aboutus = () => {
+  let { search } = useLocation();
+  const searchParams = new URLSearchParams(search);
+  const IsMobileView = searchParams.get("mobile") ?? "false";
+  return (
+    <>
+      {IsMobileView !== "true" && <Navbar />}
+      <Container maxW={"container.xl"} alignContent={"flex-start"}>
+        <BreadCrumbCom second={"About Us"} secondUrl={"/about-us"} />{" "}
+      </Container>
+      <Container maxW={"container.xl"} mb={4} px={0}>
+        <Image src={require("../assets/About us/about-us.jpg")} />
+        <Flex flexDirection={"column"} gap={3} px={20} py={10}>
+          <Image
+            src={require("../assets/About us/WhatsApp Image 2020-05-15 at 6.27.36 PM (1).jpeg")}
+          />
+          <Box
+            fontWeight={"600"}
+            color="text.500"
+            fontSize={"27px"}
+            textAlign={"center"}
+          >
+            About Sat Aushadh
+          </Box>
+
+          <Box maxW={"6xl"} color={"text.300"} textAlign={"justify"}>
+            <b>SAT AUSHADH</b> are artisan blends, infused with herbs consistent
+            with Ayurvedic principles and mixed in perfect proportions to give
+            you a refreshing delectable taste.
+            <br />
+            <br />
+            When you buy this blend , you also support ethical farming
+            practices, and farmers who work hard under tough hilly or
+            mountainous conditions to bring you authentic products without
+            harming Mother Nature.
+            <br />
+            <br />
+            SAT <b>AUSHADH</b> draws inspiration from "Bansi Gir Gaushala", and
+            its work towards reviving Bharat’s ancient “Gau Sanskriti”. We
+            believe ancient Bharat holds the solution to many of the challenges
+            facing humanity today.
+            <br />
+            <br />
+            SAT <b>AUSHADH</b> mission is to change the way people think about
+            food and beverage, bringing simple Ayurvedic wisdom back into
+            people’s lives. Our brand aims to recreate the same purity and
+            authenticity that is characteristic of ancient Bharat. While doing
+            so, we help people empower farmers who are the cornerstone of
+            "Bharatiya Gau Sanskriti"
+            <br />
+            <br />
+          </Box>
+          <Heading textAlign={"center"} color={"text.500"} fontSize={"3xl"}>
+            Know more About our Parent Company
+          </Heading>
+          <Image
+            mx={"auto"}
+            w={"80%"}
+            src={require("../assets/About us/suryan organic dada.jpg")}
+          />
+          <Text color={"text.300"} my={8} textAlign={"center"} >
+            Our parent Suryan Organic has more than 10 years of natural farming,
+            food research and development experience. We are associated with
+            over 5,000 ethical and naturally growing trusted farmers, from all
+            over BHARAT. We believe that if we have to make a difference in
+            society, our influence must extend from the farm all the way to the
+            urban household
+          </Text>
+          <Button
+          fontWeight={700}
+          color={"brand.500"}
+          as={RouterLink}
+          to={"https://www.suryanorganic.com/"}
+          target="_blank"
+          variant={"outline"}
+          borderRadius={"10px"}
+          borderColor={"brand.500"}
+          _hover={{ bgColor: "brand.500", color: "white" }}
+          mx={"auto"}
+        >
+          Read more
+        </Button>
+        </Flex>
+        <hr />
+        <Box
+          w="100%"
+          backgroundSize="100%"
+          backgroundPosition="50% 100%"
+          backgroundRepeat={"no-repeat"}
+        >
+          <Heading
+            color="brand.500"
+            size="lg"
+            mx="auto"
+            align={"center"}
+            my={"5"}
+            pb={"10px"}
+          >
+            AVAILABLE AT
+          </Heading>
+        </Box>
+        <Container maxW={"container.xl"} mb={5} centerContent>
+          <Image
+            src={require("../assets/HomaPage/our-stores.jpg")}
+            w={"container.xl"}
+            alt=""
+            style={{
+              opacity: 1,
+              transition: "opacity 0.7s", // Note the corrected syntax here
+            }}
+          />
+        </Container>
+      </Container>
+      {IsMobileView !== "true" && <Footer />}
+    </>
+  );
+};
+
+export default Aboutus;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,496 @@
+import { useState, useEffect } from "react";
+import Loader from "../components/Loader";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Carousel from "../components/Carousel";
+import CarouselWithLinks from "../components/CarouselWithLinks";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
+import ProductListSectionHome from "../components/ProductListSectionHome";
+
+import {
+  Container,
+  Flex,
+  Image,
+  Heading,
+  Stat,
+  StatNumber,
+  StatHelpText,
+  SimpleGrid,
+  Box,
+  Link,
+  Center,
+  useMediaQuery,
+  Text,
+  Grid,
+  GridItem,
+  LinkBox,
+  LinkOverlay,
+  useBreakpointValue,
+  Card,
+  Skeleton,
+  Button,
+  Divider,
+} from "@chakra-ui/react";
+import client from "../setup/axiosClient";
+import CheckOrSetUDID from "../utils/checkOrSetUDID";
+import { useNavigate, NavLink as RouterLink } from "react-router-dom";
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import Testimonials from "../components/testimonials";
+
+
+
+const images = [
+  {
+    image1: require("../assets/HomaPage/amla juice.png"),
+    id:1770
+  },
+  {
+    image1: require("../assets/HomaPage/triphala powder 22222.png"),
+    id:1783
+  },
+  {
+    image1: require("../assets/HomaPage/moringa powder.png"),
+    id:1785
+  },
+  {
+    image1: require("../assets/HomaPage/wheat grass juice.png"),
+    id:1773
+  },
+  {
+    image1: require("../assets/HomaPage/sprirulina powder.png"),
+    id:1786
+  },
+];
+
+
+
+const Licences = [
+  {
+    src:require("../assets/HomaPage/apeda (1).png"),
+    alt: "Gir Gauveda",
+  },
+  {
+    src:  require("../assets/HomaPage/fassai_2 (1).png"),
+    alt: "So Good",
+  },
+  {
+    src:require("../assets/HomaPage/MSME.png"),
+    alt: "Spices Board",
+  },
+  {
+    src:require("../assets/HomaPage/spices_board (1).png"),
+    alt: "Himalayan Mountain",
+  },
+  {
+    src: require("../assets/HomaPage/lPCR_logo (1).png"),
+    alt: "CoffeeCo",
+  },
+  {
+    src:require("../assets/HomaPage/aayush (1).png"),
+    alt: "Shishu veda",
+  },
+];
+
+const banner = [
+  {
+    id: 11,
+    alt_text: "Image3",
+    image: require("../assets/HomaPage/Satveda_banner.jpg"),
+    display_status: true,
+    image_url: null,
+  },
+
+  {
+    id: 12,
+    alt_text: "Image2",
+    image: require("../assets/HomaPage/banner 1.jpg"),
+    display_status: true,
+    image_url: null,
+  },
+
+  {
+    id: 13,
+    alt_text: "Image3",
+    image: require("../assets/HomaPage/Golden_Milk_.jpg"),
+    display_status: true,
+    image_url: null,
+  },
+  {
+    id: 14,
+    alt_text: "Image3",
+    image: require("../assets/HomaPage/BANNER 2.jpg"),
+    display_status: true,
+    image_url: null,
+  },
+];
+
+export default function Home() {
+  const [isFullScreen] = useMediaQuery("(min-width: 768px)");
+  const width = useBreakpointValue({ base: "100%", lg: "100%" });
+  const height = useBreakpointValue({ base: "300", lg: "400" });
+  const [banners, setBanners] = useState(banner);
+  const [newArrival, setNewArrival] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isMobile] = useMediaQuery("(max-width: 480px)");
+  const [homeData, setHome] = useState({});
+  const [MustTry, setMustTry] = useState([]);
+  const [BestSeller, setBestSeller] = useState([]);
+  // let [isFull] = useMediaQuery("(max-width:1920px)");
+  const [blogs, setBlogs] = useState([]);
+  const isMobiles = width <= 768;
+  const navigate = useNavigate();
+  useEffect(() => {
+    CheckOrSetUDID();
+    getHomePageData();
+    getBlogs();
+    getArrival();
+    getMustTry();
+    getBestSeller();
+  }, []);
+
+  async function getHomePageData() {
+    const response = await client.get("/home");
+    if (response.data.status === true) {
+      //setBanners(response.data.banners);
+      setHome(response.data);
+    }
+    setLoading(false);
+  }
+  async function getBlogs() {
+    const params = {};
+    const response = await client.get("/home/blogs/", {
+      params: params,
+    });
+    if (response.data.status === true) {
+      setBlogs(response.data.blogs);
+    }
+  }
+
+  async function getArrival () {
+    const response = await client.get("newarrival/list")
+      if(response) {
+        setNewArrival(response.data.data)
+      }
+      setLoading(false);
+    }
+  
+    async function getMustTry () {
+      const response = await client.get("musttry/list")
+        if(response) {
+          setMustTry(response.data.data)
+        }
+        setLoading(false);
+      }
+      async function getBestSeller () {
+        const response = await client.get("bestofalltime/list")
+          if(response) {
+            setBestSeller(response.data.data)
+          }
+          setLoading(false);
+        }
+  return (
+    <>
+      {/* {loading === true ? (
+        <Center h="100vh" w="100vw" backgroundColor={"bg.500"}>
+          <Loader site={true} />
+        </Center>
+      ) : (
+        <> */}
+      <Navbar />
+      <Container maxW={"container.xl"} px={0}>
+        {loading === true ? (
+          <Skeleton h={489}></Skeleton>
+        ) : (
+          <Carousel banners={banners} />
+        )}
+      </Container>
+
+      <Container maxW={"container.xl"} mb={8} mt={2} px={0}>
+        <Text
+          fontSize={{ base: "xl", sm: "2xl", xl: "2xl" }}
+          fontWeight={500}
+          bgColor={"bg.500"}
+          color={"text.500"}
+          textAlign={{ base: "center", md: "start" }}
+          px={{ base: 2, md: 8 }}
+          py={4}
+          my={7}
+        >
+          SAT AUSHADH
+        </Text>
+        <Text
+          color={"text.300"}
+          align={{ base: "justify", md: "center" }}
+          px={{ base: 15, lg: 20 }}
+          fontSize={{ base: "sm", lg: "lg" }}
+        >
+          <b>SAT Aushadh</b> is inspired by Bansi Gir Gaushala, and its mission
+          to revive Bharat's ancient 'Gau Sanskriti', with its boundless gifts
+          that bring joy and healing to ailing humanity. 'Sat' stands for Truth
+          or pure essence, and 'Veda' for Knowledge. Inspired by ancient
+          Ayurvedic wisdom, SatAushadh aims to bring you herbs of the highest
+          standards of purity and authenticity.
+          <br />
+          <br />
+          <b>SAT Aushadh</b> is supporting, "Siddha Kisan Se" which is on a
+          mission to procure directly from ethical and trusted farmers of Bharat
+          and further supply such naturally grown food and herbs to every home.
+          <br />
+          <br />
+        </Text>
+        <Button
+          fontWeight={700}
+          color={"brand.500"}
+          as={RouterLink}
+          to={"/about-us"}
+          variant={"outline"}
+          borderRadius={"10px"}
+          borderColor={"brand.500"}
+          _hover={{ bgColor: "brand.500", color: "white" }}
+          mx={"45%"}
+        >
+          Read more
+        </Button>
+      </Container>
+
+      <Container mb={5} px={0} maxW={"container.xl"} centerContent>
+        <LazyLoadImage
+          src={require("../assets/HomaPage/Sat_Ausadh.jpg")}
+          alt=""
+          style={{
+            opacity: 1,
+            transition: "opacity 0.7s", // Note the corrected syntax here
+          }}
+        />
+      </Container>
+      <Container mb={5} px={{xl:"10%"}} maxW={"container.xl"} centerContent>
+        <Image mb={12} src={require("../assets/HomaPage/sat_aushadh.jpg")} cursor={"pointer"} onClick={()=>navigate("/products/1783")} />
+        {images.map((data) => (
+          <>
+          <Image src={require("../assets/HomaPage/page breker.png")} />
+          <Image mt={8} src={data.image1} cursor={"pointer"} onClick={()=>navigate(`/products/${data.id}`)} />
+          </>
+        ))}
+         <Image my={9} src={require("../assets/HomaPage/page breker.png")} />
+      </Container>
+      <Container mb={5} px={0} maxW={"container.xl"} centerContent>
+        <Image src={require("../assets/HomaPage/middle banner home page.jpg")} />
+      </Container>
+
+      <ProductListSectionHome
+        title="New Arrival"
+        loading={loading}
+        products={newArrival}
+      />
+
+      <ProductListSectionHome
+        title="MUST TRY : SAT AUSHADH Products"
+        loading={loading}
+        products={MustTry}
+      />
+
+      <ProductListSectionHome
+        title="Best Seller Of All Time"
+        loading={loading}
+        products={BestSeller}
+      />
+    
+      <Container backgroundColor={"bg.500"} maxW={"container.xl"} px={0} py={2}>
+        <SimpleGrid
+          columns={[2, 3, null, 5]}
+          px={6}
+          maxW={"container.xl"}
+          my={6}
+          backgroundColor={"bg.500"}
+          align="center"
+          spacingX={{ base: "10vw", md: "30px" }}
+          spacingY="40px"
+        >
+          <Stat>
+            <StatNumber color="text.500" fontSize={{ base: "3xl", md: "3xl" }}>
+            19+
+            </StatNumber>
+            <StatHelpText color="text.500">Natural Products</StatHelpText>
+          </Stat>
+
+          <Stat>
+            <StatNumber color="text.500" fontSize={{ base: "3xl", md: "3xl" }}>
+            11534+
+            </StatNumber>
+            <StatHelpText color="text.500">Satisfied Clients</StatHelpText>
+          </Stat>
+
+          <Stat>
+            <StatNumber color="text.500" fontSize={{ base: "3xl", md: "3xl" }}>
+            1485+
+            </StatNumber>
+            <StatHelpText color="text.500">Cities & Towns</StatHelpText>
+          </Stat>
+          <Stat>
+            <StatNumber color="text.500" fontSize={{ base: "3xl", md: "3xl" }}>
+              7+
+            </StatNumber>
+            <StatHelpText color="text.500">Countries</StatHelpText>
+          </Stat>
+
+          <Stat>
+            <StatNumber color="text.500" fontSize={{ base: "3xl", md: "3xl" }}>
+              15+
+            </StatNumber>
+            <StatHelpText color="text.500">Stores</StatHelpText>
+          </Stat>
+
+         
+        </SimpleGrid>
+      </Container>
+      <Container my={9} maxW={{ base: "100vw", md: "container.xl" }}>
+        <Box
+          w="100%"
+          backgroundSize="100%"
+          backgroundPosition="50% 100%"
+          backgroundRepeat={"no-repeat"}
+        >
+          <Heading
+            color="brand.500"
+            size="lg"
+            mx="auto"
+            align={"center"}
+            mt={3}
+          >
+            OUR CERTIFICATIONS & AWARDS
+          </Heading>
+        </Box>
+        <Text my={5} textAlign={"center"} color="text.300">
+          We are committed to quality and each of our facility is independently
+          certified by an industry-accredited agency.
+        </Text>
+        <Flex
+          justifyContent="space-evenly"
+          direction={{ base: "column", md: "row" }}
+          align="center"
+          gap={12}
+          pt={1}
+          pb={6}
+        >
+          <LazyLoadImage
+            src={
+              "https://forntend-bucket.s3.ap-south-1.amazonaws.com/sose/images/HomePage/global-certificate.jpg"
+            }
+            alt="global-certificate"
+            style={{
+              opacity: 1,
+              transition: "opacity 0.7s", // Note the corrected syntax here
+            }}
+          />
+          <LazyLoadImage
+            src={
+              "https://forntend-bucket.s3.ap-south-1.amazonaws.com/sose/images/HomePage/ciolook-certificate.jpg"
+            }
+            alt="ciolook-certificate"
+            style={{
+              opacity: 1,
+              transition: "opacity 0.7s", // Note the corrected syntax here
+            }}
+          />
+        </Flex>
+        <Box
+          w="100%"
+          backgroundSize="100%"
+          backgroundPosition="50% 100%"
+          backgroundRepeat={"no-repeat"}
+        >
+          <Heading
+            color="brand.500"
+            size="lg"
+            mx="auto"
+            align={"center"}
+            my={5}
+            pb={"10px"}
+          >
+            LICENCES & AFFILIATION
+          </Heading>
+        </Box>
+        <Grid
+          templateColumns={{
+            base: "repeat(1, 1fr)",
+            md: "repeat(3, 1fr)",
+            xl: "repeat(6, 1fr)",
+          }}
+          gap={6}
+          my={10}
+          px={{xl:"12%",md:10}}
+        >
+          {Licences.map((data) => (
+            <GridItem>
+              <Image src={data.src}  />
+            </GridItem>
+          ))}
+        </Grid>
+         <Image mx={"auto"} src={require("../assets/HomaPage/sataushadh(1).jpg")} w={"70%"} />
+        <Box
+          w="100%"
+          backgroundSize="100%"
+          backgroundPosition="50% 100%"
+          backgroundRepeat={"no-repeat"}
+        >
+          <Heading
+            color="brand.500"
+            size="lg"
+            mx="auto"
+            align={"center"}
+            my={7}
+            pb={"10px"}
+          >
+            SERVING TO THE COUNTRIES
+          </Heading>
+        </Box>
+        <Box display={"flex"} justifyContent={"center"}>
+          <LazyLoadImage
+            src={require("../assets/HomaPage/Map-Yl1Weys_.webp")}
+            w={{ base: "100%", md: "100%" }}
+            alt=""
+            py={4}
+            style={{
+              opacity: 1,
+              transition: "opacity 0.7s", // Note the corrected syntax here
+            }}
+          />
+        </Box>
+        <Box
+          w="100%"
+          backgroundSize="100%"
+          backgroundPosition="50% 100%"
+          backgroundRepeat={"no-repeat"}
+        >
+          <Heading
+            color="brand.500"
+            size="lg"
+            mx="auto"
+            align={"center"}
+            my={"5"}
+            pb={"10px"}
+          >
+            AVAILABLE AT
+          </Heading>
+        </Box>
+        <Container maxW={"container.xl"} mb={5} px={0} centerContent>
+          <Image
+            src={
+              "https://forntend-bucket.s3.ap-south-1.amazonaws.com/sose/images/01.jpg"
+            }
+            w={"container.xl"}
+            alt=""
+            style={{
+              opacity: 1,
+              transition: "opacity 0.7s", // Note the corrected syntax here
+            }}
+          />
+        </Container>
+      </Container>
+      <Footer />
+      {/* </>
+      )} */}
+    </>
+  );
+}
