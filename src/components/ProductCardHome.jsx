6f2cc024b48cf42f02ab74581bc84f54a3bb1fbc
princesaubhagyam <prince@saubhagyam.org>
2024-12-10 19:18:38 +0530

    helmet code add

--- src/components/ProductCardHome.jsx
+++ src/components/ProductCardHome.jsx
@@ -33,7 +33,7 @@ const ProductCardHome = ({ product }) => {
             src={product.product?.home_image ? product.product?.home_image : product.product?.image1}
             alt={product.product?.name}
             borderRadius="lg"
-            boxSize="150px"
+            boxSize="200px"
             objectFit={"contain"}
             mx="auto"
           />
